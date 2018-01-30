1323dc09d5d5498a10aa55d645c3b65fdb3d049b
Simona Domnisoru <simona.domnisoru@odigeo.org>
2018-01-30 15:32:27 +0100

    add redux router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,47 @@
-import React, { Component } from "react";
-import Login from "./Login";
-import Videos from "./Videos";
-
+import React, { Component } from 'react';
+import Login from './Login';
+import Videos from './Videos';
+import actionTypes from './store/actionTypes';
+import store from './store/store';
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      pageToDisplay: "page_login",
-      sessionId: ""
+    constructor(props) {
+        super(props);
+        this.state = {
+            pageToDisplay: store.getState().pageToDisplay,
+            sessionId: ''
+        };
+    }
+    handleChangePage = newPage => {
+        store.dispatch({ type: newPage });
+        this.setState({ pageToDisplay: store.getState().pageToDisplay });
     };
-  }
-  handleChangePage = newPage => {
-    this.setState({ pageToDisplay: newPage });
-  };
-  handleSessionId = sessionId => {
-    this.setState({ sessionId: sessionId });
-  };
-  render() {
-    var pageToDisplay = this.state.pageToDisplay;
+    handleSessionId = sessionId => {
+        this.setState({ sessionId: sessionId });
+    };
 
-    return (
-      <div class="app-body">
-        {pageToDisplay === "page_login" ? (
-          <Login
-            pageToDisplay={this.state.pageToDisplay}
-            onChangePage={this.handleChangePage}
-            onSetSessionId={this.handleSessionId}
-          />
-        ) : pageToDisplay === "page_videos" ? (
-          <Videos sessionId={this.state.sessionId} />
-        ) : null}
-      </div>
-    );
-  }
+    renderPage = () => {
+        switch (this.state.pageToDisplay) {
+            case actionTypes.LOGIN:
+                return <Login
+                    onChangePage={this.handleChangePage}
+                    onSetSessionId={this.handleSessionId}
+                />
+                break;
+            case actionTypes.VIDEOS:
+                return <Videos sessionId={this.state.sessionId} />
+                break;
+            default:
+                return null;
+        }
+    }
+    render() {
+        var pageToDisplay = this.state.pageToDisplay;
+
+        return (
+            <div className='app-body'>
+                {this.renderPage()}
+            </div>
+        );
+    }
 }
 export default App;
